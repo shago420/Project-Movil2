f27495fcd38b3ba0b9d6a0ba7643976cd6846110
shago420 <shago123-@hotmail.com>
2024-04-08 07:25:00 -0500

    # El numeral es para MyPHPServer

--- config/db.py
+++ config/db.py
@@ -4,14 +4,14 @@ from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/tasksul'
-user = "winsignares"
-password = "admin123"
-direc = "winsignares.mysql.pythonanywhere-services.com"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/tasksul'
+#user = "winsignares"
+#password = "admin123"
+#direc = "winsignares.mysql.pythonanywhere-services.com"
 namebd = "tasksul"
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{user}:{password}@{direc}/{namebd}'
-app.config['SQLALCHEMY_TRACK_NOTIFICACTIONS'] = False
-app.secret_key = "Movil2"
+#app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{user}:{password}@{direc}/{namebd}'
+#app.config['SQLALCHEMY_TRACK_NOTIFICACTIONS'] = False
+#app.secret_key = "Movil2"
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
